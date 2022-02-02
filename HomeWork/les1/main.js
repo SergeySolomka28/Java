81c8148a490232dd79d1496404d53ea79038bac9
SeregaSolomka28 <1994solomka@gmail.com>
2022-02-03 01:01:33 +0200

    закончил дз

--- HomeWork/les1/main.js
+++ HomeWork/les1/main.js
@@ -1,45 +1,46 @@
-let a =('heloo');
-let b =('owu');
+let a ='heloo';
+a='rty';
+let b ='owu';
+b='cdd';
 let c =('com');
+c='sss';
 let d =('ua');
+d= ='hf';
 let e =(1);
+e=23;
 let i =(10);
+i=32;
 let f =(-999);
+f=39;
 let g =(123);
+g=90;
 let h =(3.14);
+h=88;
 let k =(2.7);
+k=63;
 let l =(16);
+l='jfj';
 let m = true;
+m='ryu';
 let n =false;
+n=true;
 //
-console.log(a);
-console.log(b);
-console.log(c);
-console.log(d);
-console.log(e);
-console.log(i);
-console.log(f);
-console.log(g);
-console.log(h);
-console.log(k);
-console.log(l);
-console.log(m);
-console.log(n);
-//
-//alert(a);
-//alert(b);
-//alert(c);
-//alert(d);
-//alert(e);
-//alert(f);
-//alert(g);
-//alert(h);
-//alert(k);
-//alert(l);
-//alert(m);
-//alert(n);
-//alert(i);
-//
+console.log(a,b,c,d,e,i,f,g,h,k,l,m,n );
+
+alert(a);
+alert(b);
+alert(c);
+alert(d);
+alert(e);
+alert(f);
+alert(g);
+alert(h);
+alert(k);
+alert(l);
+alert(m);
+alert(n);
+alert(i);
+
 document.write(a);
 document.write(b);
 document.write(c);
@@ -53,6 +54,47 @@ document.write(k);
 document.write(l);
 document.write(m);
 document.write(n);
+
+//
+let fistname=('Solomka');
+let middlename=('Serhii');
+let lastname=('Oleksanrovych');
+
+let person = (fistname+middlename+lastname);
+console.log(person);
+let name = prompt("Имя?");
+let sername = prompt("Отчество?");
+let age = prompt("Твой возраст?");
+console.log('Congratulations', name,sername, 'your age',age );
+
+//В однакових виразах не використовувати однакові оператори!!!
+//5 ? 6 -> true
+//5 ? 6 -> false
+//5 ? 6 -> false
+//5 ? 6 -> false
+//10 ? 10 -> true
+//10 ? 10 -> true
+//10 ? 10 -> false
+//10 ? 10 -> false
+//10 ? 10 -> false
+//123 ? '123' -> false
+//123 ? '123' -> true
+
+console.log(5<6);
+console.log(5>6);
+console.log(5>=6);
+console.log(5===6);
+console.log(10===10);
+console.log(10>=10);
+console.log(10>10);
+console.log(10<10);
+console.log(123==='123');
+console.log(123!=='123');
+
+
+
+
+
 
 
 
