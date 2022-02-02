2a17799dfbaabdcc1938ebc15c393432b0345731
SeregaSolomka28 <1994solomka@gmail.com>
2022-02-02 02:11:20 +0200

    ClassWork

---
+++ ClassWork/main.js
@@ -0,0 +1,44 @@
+
+let mas = [12,433,343,55,63,23,43,11,58,76];
+let result = (mas[0]+mas[1]+mas[2]+mas[3]+mas[4]+mas[5]
+    +mas[6]+mas[7]+mas[8]+mas[9]);
+console.log(result);
+//
+let book = {
+    name:'TheWitcher2',
+    pages: 346,
+    genre: 'adventure'
+}
+console.log(book);
+let book2 = {
+    name:'TheWitcher',
+    pages: 234,
+    genre: 'adventure',
+    author: 'Andzhey Sapkovskiy'
+}
+console.log(book2);
+//
+let books = [{book},{book2}]
+console.log(books[0]);
+console.log(books[1]);
+//
+let width =(10);
+let height =(23);
+let s = (width*height);
+console.log(s);
+//
+let height1 = (10);
+let dc=(4);
+let p=(3.14);
+let v=(height1*p*dc**2/4);
+console.log(v);
+//
+
+let g =(Math.pow(3,2)*Math.pow(4,2));
+let k =(Math.sqrt(g));
+console.log(k);
+
+
+
+
+
