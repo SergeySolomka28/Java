337b39831eab7431cfa3d3248ab6cf13c760d990
SeregaSolomka28 <1994solomka@gmail.com>
2022-02-02 03:03:36 +0200

    homework start

---
+++ HomeWork/les1/main.js
@@ -0,0 +1,58 @@
+let a =('heloo');
+let b =('owu');
+let c =('com');
+let d =('ua');
+let e =(1);
+let i =(10);
+let f =(-999);
+let g =(123);
+let h =(3.14);
+let k =(2.7);
+let l =(16);
+let m = true;
+let n =false;
+//
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
+console.log(i);
+console.log(f);
+console.log(g);
+console.log(h);
+console.log(k);
+console.log(l);
+console.log(m);
+console.log(n);
+//
+//alert(a);
+//alert(b);
+//alert(c);
+//alert(d);
+//alert(e);
+//alert(f);
+//alert(g);
+//alert(h);
+//alert(k);
+//alert(l);
+//alert(m);
+//alert(n);
+//alert(i);
+//
+document.write(a);
+document.write(b);
+document.write(c);
+document.write(d);
+document.write(e);
+document.write(i);
+document.write(f);
+document.write(g);
+document.write(h);
+document.write(k);
+document.write(l);
+document.write(m);
+document.write(n);
+
+
+
